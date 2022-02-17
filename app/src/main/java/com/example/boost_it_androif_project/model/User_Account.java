c2796703efc2f8c7fcddf74d79566b5f2df38e27
JonathanAlkalay <alkalayjonathan28@yahoo.com>
2022-02-17 20:07:51 +0200

    added layout screens and different classes

---
+++ app/src/main/java/com/example/boost_it_androif_project/model/User_Account.java
@@ -0,0 +1,27 @@
+package com.example.boost_it_androif_project.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User_Account extends Account{
+
+    List<post> savedPosts;
+
+    public User_Account() {
+        super();
+        this.savedPosts = new ArrayList<post>();
+    }
+
+    public User_Account(String firstName, String lastName, String phoneNumber, String email, Boolean loggedIn, List<post> savedPosts) {
+        super(firstName, lastName, phoneNumber, email, loggedIn);
+        this.savedPosts = savedPosts;
+    }
+
+    public List<post> getSavedPosts() {
+        return savedPosts;
+    }
+
+    public void setSavedPosts(List<post> savedPosts) {
+        this.savedPosts = savedPosts;
+    }
+}
